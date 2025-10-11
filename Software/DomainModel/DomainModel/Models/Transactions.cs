ffc780c4f0ceb2143f14bedb0d6128ef8a83af2a
ivanNavialic <inavialic22@student.foi.hr>
2025-10-11 08:27:30 +0200

    Add files via upload

---
+++ Software/DomainModel/DomainModel/Models/Transactions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Models
+{
+    public class Transactions
+    {
+        public int TransactionId { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public decimal Amount { get; set; }
+        public string Type { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
